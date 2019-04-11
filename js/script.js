d3cf9629b3d0840ea11b3b81eaf2500bced55e40
Thomas <tfracassi@hawk.iit.edu>
2019-04-11 15:56:45 -0500

    add js for Lab7 variables and function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function init(){
 //add your javascrip between these two lines of code
- 
 
+  function processClickFunction(){
+    alert("Thomas Fracassi: " + inputvar.value);
+    document.getElementById("textoutput").innerHTML = inputvar.value;
+  }
 
+  var buttonvar = document.getElementById('entrybutton');
+  var inputvar = document.getElementById('entryinput');
+  
+  buttonvar.addEventListener('click', processClickFunction);
 
-
-
-
-
+}
 window.addEventListener('load', init);
